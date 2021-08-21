5328d3000fffef6318a3120a0f603b792a95a5ed
allenbenny419 <allenjosebenny@gmail.com>
2021-08-21 11:59:05 +0530

    Sum of n natural numbers

---
+++ Basics/sum_of_naturalno.cpp
@@ -0,0 +1,13 @@
+  // Sum of n natural numbers 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter A Number :"<<endl;
+    cin>>N;
+    cout<<"Sum of Numbers from 1 to "<<N<<" = ";
+    cout<<((N*(N+1))/2);
+    return 0;
+}
